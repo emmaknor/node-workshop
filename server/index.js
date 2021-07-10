b8778745e16af02c2ed69bf3eb57674b011de744
Emma <knoremma@gmail.com>
2021-07-10 16:25:38 -0600

    remove library related routes

--- server/index.js
+++ server/index.js
@@ -15,56 +15,9 @@ app.use(express.json());
 // use cors middleware for enabling CORS with various options
 app.use(cors());
 
-// routes
-app.get('/authors/:id', (req, res, next) => {
-  let authorId = req.params.id;
-  queries.getAuthors(authorId, (err, authors) => {
-    if (err) {
-      res.status(404).send(err);
-    } else {
-      res.status(200).send(authors);
-    }
-  })
-});
-
-app.post('/book', (req, res) => {
-  let book = req.body;
-  queries.addBook(book, (err, results) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201).send('book successfully added!');
-    }
-  })
-});
-
-app.patch('/author/:id', (req, res) => {
-  queries.updateAuthor(req.params.id, req.body.name, (err, results) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201).send('name successfully updated!');
-      // console.log(results)
-    }
-  })
-});
-
-app.delete('/book', (req, res) => {
-  let title = req.query.bookTitle;
-  console.log(title);
-  queries.deleteBook(title, (err, results) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send('book deleted');
-    }
-  })
-});
+// routes go here :)
 
 // tell server to listen on predefined port
 app.listen(PORT, () => {
   console.log(`Express listenting on port ${PORT}`);
 });
-
-/* curl -d '{"name":"Truman Purnell"}' -H "Content-Type: application/json" -X PATCH http://localhost:3000/author/7
-curl -X DELETE http://localhost:3000/book?bookTitle=Gone%20Girl */
